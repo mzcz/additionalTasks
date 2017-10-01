98c865c5a6913ec7c381058bb66b728645693b6b
mzcz <mzcz@wp.pl>
2017-10-01 21:49:44 +0200

    Additional tasks

---
+++ src/main/java/com/additionaltasks/AdditionalTasksApplication.java
@@ -0,0 +1,12 @@
+package com.additionaltasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdditionalTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdditionalTasksApplication.class, args);
+	}
+}
